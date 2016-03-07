2fec08b6411df8cd65de684401322c0ef9cff3ec
Roderick <2419011393@qq.com>
2016-03-07 11:20:59 +0800

    提现模块完成

--- gjtjb/RKWXT/NewUserCut/withdrawals/Model/ApplyAliModel.m
+++ gjtjb/RKWXT/NewUserCut/withdrawals/Model/ApplyAliModel.m
@@ -10,10 +10,44 @@
 #import "WXTURLFeedOBJ+NewData.h"
 
 @implementation ApplyAliModel
+/*
+ 接口名称：用户申请提现
+ 接口地址：https://oldyun.67call.com/wx10api/V1/user_withdraw.php
+ 请求方式：POST
+ 输入参数：
+ pid:平台类型(android,ios,web),
+ ver:版本号,
+ ts:时间戳,
+ sid : 商家ID
+ woxin_id : 我信ID
+ phone:登录的手机号
+ amount : 提现金额
+ sign: 签名
+ */
 
 -(void)applyAliMoney:(CGFloat)money{
     WXTUserOBJ *userObj = [WXTUserOBJ sharedUserOBJ];
-    NSDictionary *dic = [NSDictionary dictionaryWithObjectsAndKeys:@"iOS", @"pid", [UtilTool currentVersion], @"ver", [NSNumber numberWithInt:(int)[UtilTool timeChange]], @"ts", userObj.wxtID, @"woxin_id", [NSNumber numberWithFloat:(float)money], @"amount", nil];
+//    NSDictionary *dic = [NSDictionary dictionaryWithObjectsAndKeys:@"iOS", @"pid", [UtilTool currentVersion], @"ver", [NSNumber numberWithInt:(int)[UtilTool timeChange]], @"ts", userObj.wxtID, @"woxin_id", [NSNumber numberWithFloat:(float)money], @"amount", nil];
+    
+    NSMutableDictionary *baseDic = [NSMutableDictionary dictionary];
+    baseDic[@"pid"]= @"ios";
+    baseDic[@"ver"]= [UtilTool currentVersion];
+    baseDic[@"ts"]= [NSNumber numberWithInt:(int)[UtilTool timeChange]];
+    baseDic[@"woxin_id"]= userObj.wxtID;
+    baseDic[@"phone"]= userObj.user;
+    baseDic[@"sid"]= [NSNumber numberWithInt:(int)kMerchantID];
+    baseDic[@"amount"]= [NSNumber numberWithFloat:(float)money];
+    
+    NSMutableDictionary *dic = [NSMutableDictionary dictionary];
+    dic[@"pid"]= @"ios";
+    dic[@"ver"]= [UtilTool currentVersion];
+    dic[@"ts"]= [NSNumber numberWithInt:(int)[UtilTool timeChange]];
+    dic[@"woxin_id"]= userObj.wxtID;
+    dic[@"phone"]= userObj.user;
+    dic[@"sid"]= [NSNumber numberWithInt:(int)kMerchantID];
+    dic[@"amount"]= [NSNumber numberWithFloat:(float)money];
+    dic[@"sign"]= [UtilTool md5:[UtilTool allPostStringMd5:baseDic]];
+    
     [[WXTURLFeedOBJ sharedURLFeedOBJ] fetchNewDataFromFeedType:WXT_UrlFeed_Type_New_ApplyAliMoney httpMethod:WXT_HttpMethod_Post timeoutIntervcal:-1 feed:dic completion:^(URLFeedData *retData) {
         if(retData.code != 0){
             if(_delegate && [_delegate respondsToSelector:@selector(applyAliMoneyFailed:)]){
--- gjtjb/RKWXT/NewUserCut/withdrawals/Model/ConfirmUserAliModel.m
+++ gjtjb/RKWXT/NewUserCut/withdrawals/Model/ConfirmUserAliModel.m
@@ -11,9 +11,56 @@
 
 @implementation ConfirmUserAliModel
 
+/*
+ 接口名称：设置提现账号
+ 接口地址：https://oldyun.67call.com/wx10api/V1/set_withdraw_account.php
+ 请求方式：POST
+ 输入参数：
+ pid:平台类型(android,ios,web),
+ ver:版本号,
+ ts:时间戳,
+ sid : 商家ID
+ woxin_id : 我信ID
+ phone:登录的手机号
+ account : 支付宝账号
+ username : 支付宝姓名
+ type:操作类型  1.添加  2.修改
+ rand_id:验证码ID
+ rcode:验证码
+ sign: 签名
+ */
+
 -(void)confirmUserAliAcountWith:(NSString *)userAliAcount with:(NSString *)userName with:(UserAli_Submit)aliType with:(NSInteger)rcode with:(NSString *)userPhone{
     WXTUserOBJ *userObj = [WXTUserOBJ sharedUserOBJ];
-    NSDictionary *dic = [NSDictionary dictionaryWithObjectsAndKeys:@"iOS", @"pid", [UtilTool currentVersion], @"ver", [NSNumber numberWithInt:(int)[UtilTool timeChange]], @"ts", userObj.wxtID, @"woxin_id", userAliAcount, @"account", userName, @"username", [NSNumber numberWithInt:(int)aliType], @"type", [NSNumber numberWithInt:(int)rcode], @"rcode", [NSNumber numberWithInt:(int)userObj.smsID], @"rand_id", userPhone, @"phone", nil];
+//    NSDictionary *dic = [NSDictionary dictionaryWithObjectsAndKeys:@"iOS", @"pid", [UtilTool currentVersion], @"ver", [NSNumber numberWithInt:(int)[UtilTool timeChange]], @"ts", userObj.wxtID, @"woxin_id", userAliAcount, @"account", userName, @"username", [NSNumber numberWithInt:(int)aliType], @"type", [NSNumber numberWithInt:(int)rcode], @"rcode", [NSNumber numberWithInt:(int)userObj.smsID], @"rand_id", userPhone, @"phone", nil];
+    
+    NSMutableDictionary *baseDic = [NSMutableDictionary dictionary];
+    baseDic[@"pid"]= @"ios";
+    baseDic[@"ver"]= [UtilTool currentVersion];
+    baseDic[@"ts"]= [NSNumber numberWithInt:(int)[UtilTool timeChange]];
+    baseDic[@"woxin_id"]= userObj.wxtID;
+    baseDic[@"phone"]= userObj.user;
+    baseDic[@"sid"]= [NSNumber numberWithInt:(int)kMerchantID];
+    baseDic[@"account"]= userAliAcount;
+    baseDic[@"username"]= userName;
+    baseDic[@"type"]= [NSNumber numberWithInt:(int)aliType];
+    baseDic[@"rand_id"]= [NSNumber numberWithInt:(int)userObj.smsID];
+    baseDic[@"rcode"]= [NSNumber numberWithInt:(int)rcode];
+    
+    NSMutableDictionary *dic = [NSMutableDictionary dictionary];
+    dic[@"pid"]= @"ios";
+    dic[@"ver"]= [UtilTool currentVersion];
+    dic[@"ts"]= [NSNumber numberWithInt:(int)[UtilTool timeChange]];
+    dic[@"woxin_id"]= userObj.wxtID;
+    dic[@"phone"]= userObj.user;
+    dic[@"sid"]= [NSNumber numberWithInt:(int)kMerchantID];
+    dic[@"account"]= userAliAcount;
+    dic[@"username"]= userName;
+    dic[@"type"]= [NSNumber numberWithInt:(int)aliType];
+    dic[@"rand_id"]= [NSNumber numberWithInt:(int)userObj.smsID];
+    dic[@"rcode"]= [NSNumber numberWithInt:(int)rcode];
+    dic[@"sign"]= [UtilTool md5:[UtilTool allPostStringMd5:baseDic]];
+    
     [[WXTURLFeedOBJ sharedURLFeedOBJ] fetchNewDataFromFeedType:WXT_UrlFeed_Type_New_SubmitUserAliAcount httpMethod:WXT_HttpMethod_Post timeoutIntervcal:-1 feed:dic completion:^(URLFeedData *retData) {
         if(retData.code != 0){
             if(_delegate && [_delegate respondsToSelector:@selector(confirmUserAliAcountFailed:)]){
